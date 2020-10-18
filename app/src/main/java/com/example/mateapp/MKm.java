197f501fc5c2beeb55a91d240352c8dc488f13b5
MarcosVel <marcos.veloso@sempreceub.com>
2020-10-18 17:26:39 -0300

    Adiconando função de Metros para KM

--- app/src/main/java/com/example/mateapp/MKm.java
+++ app/src/main/java/com/example/mateapp/MKm.java
@@ -3,6 +3,9 @@ package com.example.mateapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MKm extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MKm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_m_km);
+
+        final EditText edtMProg = (EditText) findViewById(R.id.edtMetro);
+        final EditText edtKmProg = (EditText) findViewById(R.id.edtKm);
+        Button btnConverterProg = (Button) findViewById(R.id.btnConverter);
+
+        btnConverterProg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float m, km;
+                m = Float.parseFloat(edtMProg.getText().toString());
+                km = m / 1000;
+                edtKmProg.setText(String.valueOf(km));
+            }
+        });
+
     }
 }
