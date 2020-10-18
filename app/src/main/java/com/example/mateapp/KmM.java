f0bbc666f50cbd03e6188be46c87a8efaee592d3
MarcosVel <marcos.veloso@sempreceub.com>
2020-10-18 16:05:39 -0300

    Adicionando novas activitys e ligando elas na main

---
+++ app/src/main/java/com/example/mateapp/KmM.java
@@ -0,0 +1,14 @@
+package com.example.mateapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KmM extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_km_m);
+    }
+}
--- app/src/main/java/com/example/mateapp/MainActivity.java
+++ app/src/main/java/com/example/mateapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.mateapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Botão de KM para Mt
+        Button btnKmMtprog = (Button) findViewById(R.id.btnKmMt);
+        //Botão de Mt para KM
+        Button btnMtKmprog = (Button) findViewById(R.id.btnMtKm);
+
+        btnKmMtprog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, KmM.class);
+                startActivity(intent);
+            }
+        });
+
+        btnMtKmprog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MKm.class);
+                startActivity(intent);
+            }
+        });
     }
 }
